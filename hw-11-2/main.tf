befa7ac4d150f8cd985798ff9cba546f5ff0a0ce
GolgoPA <120748498+GolgoPA@users.noreply.github.com>
2023-04-03 13:44:39 +0300

    Add files via upload

---
+++ hw-11-2/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token = "y0_AgAAAABEezXuAATuwQAAAADXV8wjHipDbw-1ScaUnNdqKClO2Z3Ykoo"
+  cloud_id = "b1gm7bp2grqbho73ke1l"
+  folder_id = "b1g7l8ost873t9a9r3g3"
+  zone = "ru-central1-b"
+}
+resource "yandex_compute_instance" "vm" {
+  count = 1
+  name = "vm${count.index}"
+
+
+  resources {
+    core_fraction = 20
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8a67rb91j689dqp60h"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+  
+  metadata = {
+    user-data = "${file("./meta.yaml")}"
+  }
+
+}
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm-0" {
+  value = yandex_compute_instance.vm[0].network_interface.0.ip_address
+}
+output "external_ip_address_vm-0" {
+  value = yandex_compute_instance.vm[0].network_interface.0.nat_ip_address
+}
